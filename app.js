8ce5ab5a4c3593a9e3888284eda2a44e1cb951a0
erickdanielavalos <avaloserick43@gmail.com>
2023-11-27 00:06:29 -0800

    finished meme generator with the help of GPT 3.0

---
+++ app.js
@@ -0,0 +1,23 @@
+// const form = document.querySelector('form');
+// const div = document.querySelector('div');
+// const imgURLInput = document.getElementById("imgURL");
+// const topText = document.getElementById('top')
+// const bottomText = document.getElementById('bottom')
+// const container = document.getElementById('secondDiv')
+// form.addEventListener("submit", function(event) {
+//     event.preventDefault();
+//     const imgURL = imgURLInput.value;
+//     const img = document.createElement('img');
+//     img.setAttribute('src', imgURL);
+//     div.appendChild(img);
+//     const top = document.createElement('p')
+//     top.innerText(topText.value);
+//     const bottom = document.createElement('p')
+//     bottom.innerText(bottomText.value);
+//     container.appendChild(top)
+//     container.appendChild(bottom)
+//     imgURLInput.value = "";
+//     topText.value = '';
+//     bottomText.value = '';
+
+// });
